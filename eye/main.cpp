f3a786d9393d64ec5f8da01aca202a0581c1c70b
spyoon777 <spyoon777@gmail.com>
2022-06-09 14:17:47 +0900

    init

---
+++ eye/main.cpp
@@ -0,0 +1,9 @@
+// Copyright 2022 KOREA ELECTRONICS TECHNOLOGY INSTITUTE(KETI), All Rights Reserved.
+// Author: spyoon777
+
+#include <iostream>
+
+int main() {
+    std::cout << "hello world!" << std::endl;
+    return 0;
+}
